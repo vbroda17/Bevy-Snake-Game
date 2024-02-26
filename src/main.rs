90f2e4b525183546d193aa438e65636a97492a41
Vincent Broda <101155730+vbroda17@users.noreply.github.com>
2024-02-25 23:37:24 -0500

    Got user input

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,14 @@ fn main() {
                 .build(),
         )
         .add_systems(Startup, setup)
-        // .add_systems(Update, character_movement)
+        .add_systems(Update, update_direction)
         // .add_systems(Update, move_snake_segments)
         // .add_systems(Update, spawn_food)
         // .add_systems(Update, food_check)
         .run();
 }
 
+#[derive(PartialEq, Clone)]
 enum Direction{
     Up,
     Down,
@@ -36,16 +37,21 @@ pub struct Snake {
     pub speed: u32,
     pub score: u32,
     pub size: u32,
-
-    pub snake_segments: Vec<SnakeSegment>,
 }
 
 #[derive(Component)]
-pub struct SnakeSegment {
-    pub offset: u32,
-    // pub position: Vec
+pub struct SnakeHead {
+    pub visual: Entity,
     pub direction: Direction,
-    pub direction_queue: Vec<Vec2>,
+    pub previous_directions: Vec<Direction>,
+}
+
+#[derive(Component)]
+pub struct SnakeBody {
+    pub visual: Entity,
+    pub index: u32,
+    pub direction: Direction,
+    pub next_directions: Vec<Direction>,
 }
 
 fn setup(mut commands: Commands) {
@@ -55,31 +61,78 @@ fn setup(mut commands: Commands) {
     let snake_speed = 100;
     let snake_color = Color::rgb(0.1, 0.8, 0.0);
 
-    let snake_head = SnakeSegment {
-        offset: 0,
-        direction: Direction::Up,
-        direction_queue: Vec::new(),
-    };
+    let snake = commands.spawn(
+        Snake {
+            speed: snake_speed,
+            score: 0,
+            size: 10,
+        }
+    ).id();
 
-    let mut initial_segement = Vec:: new();
-    initial_segement.push(snake_head);
-
-    commands.spawn((
+    let snake_head_visual = commands.spawn(
         SpriteBundle {
             sprite: Sprite {
                 color: snake_color,
                 custom_size: snake_size,
                 ..default()
             },
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(1.0, 1.0, 1.0),
+                ..default()
+            },
             ..default()
-        },
-        Snake {
-            speed: snake_speed,
-            score: 0,
-            size: snake_size.unwrap().x as u32,
-            snake_segments: initial_segement,
         }
-    ));
+    ).id();
 
+    let snake_head = commands.spawn(
+        SnakeHead {
+            visual: snake_head_visual,
+            direction: Direction::Up,
+            previous_directions: Vec::new(),
+        }
+    );
+}
+
+fn update_direction(
+    mut commands: Commands,
+    mut snake_head: Query<&mut SnakeHead>,
+    input: Res<ButtonInput<KeyCode>>,
+){
+    for mut head in snake_head.iter_mut() {
+        let mut previous_direction = head.direction.clone();
+        
+        let new_direction = 
+            if input.pressed(KeyCode::KeyW) && previous_direction != Direction::Up {
+                println!("Now Going Up");
+                Direction::Up
+            } 
+            else if input.pressed(KeyCode::KeyS) && previous_direction != Direction::Down {
+                println!("Now Going Down");
+                Direction::Down
+            }
+            else if input.pressed(KeyCode::KeyA) && previous_direction != Direction::Left {
+                println!("Now Going Left");
+                Direction::Left
+            }
+            else if input.pressed(KeyCode::KeyD) && previous_direction != Direction::Right {
+                println!("Now Going Right");
+                Direction::Right
+            }
+            else {
+                previous_direction
+            };
+
+        head.direction = new_direction.clone();
+
+    }
+}
+
+fn move_snake_segments(){}
+
+fn add_snake_segement(
+    // mut snake: Query<&mut Snake>,
+    // mut snake_position: Query<&mut Transform, 
+){
 
 }
